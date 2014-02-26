0cd6d779cff2c55003737de6690e0250f5994d5d
David Cheung <xihua.cheung@gmail.com>
2014-02-25 21:17:40 -0500

    Initial commit. Added function to retrieve drinking activities

---
+++ app/models/CoffeeActivity.php
@@ -0,0 +1,23 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class CoffeeActivity extends Eloquent
+{
+    protected $table = 'activity';
+
+    public function getActivity($id='')
+    {
+        if ($id == ''){
+            $activity_list = CoffeeActivity::all();
+        }
+        else{
+            $activity_list = CoffeeActivity::find($id);
+        }
+        return $activity_list;
+    }
+    
+}
