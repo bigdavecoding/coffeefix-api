2035ac33a3fe009ec33c7d5b344702eaa8dd369b
David Cheung <xihua.cheung@gmail.com>
2015-03-25 17:56:33 -0700

    remove redis config

--- app/config/database.php
+++ app/config/database.php
@@ -87,9 +87,9 @@ return array(
 		'cluster' => false,
 
 		'default' => array(
-			'host'     => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_HOST),
-			'port' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_PORT),
-			'password' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_PASS),
+			'host'     => '',
+			'port' => '',
+			'password' => '',
 		),
 
 	),
